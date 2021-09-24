5f055a0063204989df3faa98e11c19105b5978f5
Anonymous275 <36374260+Anonymous-275@users.noreply.github.com>
2021-09-24 16:29:50 +0300

    stack walker

--- watchdog.cpp
+++ watchdog.cpp
@@ -4,122 +4,28 @@
 
 #include <windows.h>
 #include <imagehlp.h>
+#include <tlhelp32.h>
 #include <strsafe.h>
 #include <cstdint>
-#include "stack_string.h"
+#include <string>
+#include <atomic>
+#include <array>
 
-struct function_info {
-    void* func_address;
-    uint32_t thread_id;
-    bool enter;
-};
-
-fst::stack_string<1024> crash_file;
+std::string crash_file;
 
 template <typename I>
-fst::stack_string<(sizeof(I)<<1)+1> HexString(I w) {
+std::string HexString(I w) {
     static const char* digits = "0123456789ABCDEF";
     const size_t hex_len = sizeof(I)<<1;
-    fst::stack_string<hex_len+1> rc;
-    rc.resize(hex_len+1);
-    memset(rc.get(), '0', hex_len);
-    memset(rc.get() + hex_len, 0, 1);
+    std::string rc(hex_len, '0');
     for (size_t i=0, j=(hex_len-1)*4 ; i<hex_len; ++i,j-=4)
         rc[i] = digits[(w>>j) & 0x0f];
     return rc;
 }
 
-template<class T_>
-class heap_array {
-public:
-    heap_array() noexcept {
-        Data = (T_*)(GlobalAlloc(GPTR, Cap * sizeof(T_)));
-        init = true;
-    }
-    explicit heap_array(size_t Cap_) noexcept {
-        Cap = Cap_;
-        Data = (T_*)(GlobalAlloc(GPTR, Cap * sizeof(T_)));
-        init = true;
-    }
-    ~heap_array() {
-        GlobalFree(Data);
-    }
-    inline T_* get() noexcept {
-        return Data;
-    }
-    inline const T_* cget() noexcept {
-        return Data;
-    }
-    inline void insert(const T_& T) {
-        if(!init)return;
-        if(Size >= Cap) {
-            Grow();
-        }
-        Data[Size++] = T;
-    }
-    inline void string_insert(const T_* T, size_t len = 0) {
-        if(len == 0)len = strlen(T);
-        if(Size+len >= Cap) {
-            Grow(len);
-        }
-        memcpy(&Data[Size], T, len);
-        Size += len;
-    }
-    inline T_ at(size_t idx) {
-        return Data[idx];
-    }
-    inline size_t size() const noexcept {
-        return Size;
-    }
-    const T_& operator[](size_t idx) {
-        if (idx >= Size) {
-            throw std::exception("out of boundaries operator[]");
-        }
-        return Data[idx];
-    }
-private:
-    inline void Grow(size_t add = 0) {
-        Cap = (Cap*2) + add;
-        auto* NewData = (T_*)(GlobalAlloc(GPTR, Cap * sizeof(T_)));
-        for(size_t C = 0; C < Size; C++) {
-            NewData[C] = Data[C];
-        }
-        GlobalFree(Data);
-        Data = NewData;
-    }
-    size_t Size{0}, Cap{5};
-    bool init{false};
-    T_* Data;
-};
 
-heap_array<function_info>* watch_data;
-
-struct watchdog_mutex {
-    static void Create() noexcept {
-        hMutex = CreateMutex(nullptr, FALSE, nullptr);
-    }
-    static void Lock() {
-        WaitForSingleObject(hMutex, INFINITE);
-    }
-    static void Unlock() {
-        ReleaseMutex(hMutex);
-    }
-    struct [[nodiscard]] ScopedLock {
-        ScopedLock() {
-            if(hMutex)
-            watchdog_mutex::Lock();
-        }
-        ~ScopedLock() {
-            if(hMutex)
-            watchdog_mutex::Unlock();
-        }
-    };
-private:
-    static HANDLE hMutex;
-};
-HANDLE watchdog_mutex::hMutex{nullptr};
-std::atomic<bool> Init{false}, Sym;
 std::atomic<int64_t> Offset{0};
+std::atomic<bool> Sym{false};
 
 void watchdog_setOffset(int64_t Off) {
     Offset.store(Off);
@@ -135,52 +41,52 @@ void notify(const char* msg) {
     }
 }
 
-fst::stack_string<MAX_SYM_NAME> FindFunction(void* Address) {
+std::string getFunctionSym(DWORD64 Address) {
     if(!Sym.load()) {
-        fst::stack_string<MAX_SYM_NAME> undName;
-        return undName;
+        return {};
     }
     static HANDLE process = GetCurrentProcess();
     DWORD64 symDisplacement = 0;
-    fst::stack_string<MAX_SYM_NAME> undName;
+    std::string Result;
     TCHAR buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
     memset(&buffer,0, sizeof(buffer));
     auto pSymbolInfo = (PSYMBOL_INFO)buffer;
     pSymbolInfo->SizeOfStruct = sizeof(SYMBOL_INFO);
     pSymbolInfo->MaxNameLen	= MAX_SYM_NAME;
-    if (SymFromAddr(process, DWORD64(Address) + Offset, &symDisplacement, pSymbolInfo)) {
-        undName.push_back(pSymbolInfo->Name);
+    if (SymFromAddr(process, Address + Offset, &symDisplacement, pSymbolInfo)) {
+        Result.append(pSymbolInfo->Name);
     }
-    return undName;
+    return Result;
 }
 
 
-fst::stack_string<512> getCrashInfo(void* Address){
-    if(!Sym.load()){
-        fst::stack_string<512> Value;
-        Value.push_back("unknown", 7);
-        return Value;
+std::string getLocation(DWORD64 Address){
+    if(!Sym.load()) {
+        return "unknown";
     }
     DWORD pdwDisplacement = 0;
     IMAGEHLP_LINE64 line{sizeof(IMAGEHLP_LINE64)};
-    SymGetLineFromAddr64(GetCurrentProcess(), DWORD64(Address) + Offset, &pdwDisplacement, &line);
+    SymGetLineFromAddr64(GetCurrentProcess(), Address + Offset, &pdwDisplacement, &line);
     char* Name = nullptr;
     if(line.FileName) {
         Name = strrchr(line.FileName, '\\');
     }
-    fst::stack_string<512> Value;
-    if(Name)Value.push_back(Name+1);
-    else Value.push_back("unknown", 7);
-    char buffer[20];
-    auto n = sprintf(buffer, ":%lu", line.LineNumber);
-    Value.push_back(buffer, n);
-    return Value;
+    std::string Result;
+    if(Name){
+        Result.append(Name+1);
+        char buffer[20];
+        auto n = sprintf(buffer, ":%lu", line.LineNumber);
+        Result.append(buffer, n);
+        return Result;
+    } else {
+        return "unknown";
+    }
 }
-const char* getFunctionDetails(void* Address) {
-    return FindFunction(Address).c_str();
+std::string getFunctionDetails(size_t Address) {
+    return getFunctionSym(Address);
 }
-const char* getCrashLocation(void* Address) {
-    return getCrashInfo(Address).c_str();
+std::string getCrashLocation(size_t Address) {
+    return getLocation(Address);
 }
 
 void InitSym(const char* PDBLocation) {
@@ -188,94 +94,102 @@ void InitSym(const char* PDBLocation) {
     Sym.store(true);
 }
 
-void write_report(const char* report, size_t size) {
+void write_report(const std::string& Report) {
     HANDLE hFile = CreateFile(crash_file.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
     if (hFile == INVALID_HANDLE_VALUE) {
         notify("Failed to open crash file for writing!");
         return;
     }
     DWORD dwBytesWritten = 0;
-    auto Flag = WriteFile(hFile, report, DWORD(size), &dwBytesWritten, nullptr);
+    auto Flag = WriteFile(hFile, Report.c_str(), DWORD(Report.size()), &dwBytesWritten, nullptr);
     if (Flag == FALSE) {
         notify("Failed to write to crash file!");
     }
     CloseHandle(hFile);
 }
 
-void generate_crash_report(uint32_t Code, void* Address) {
-    watchdog_mutex::ScopedLock guard;
+std::string getStack(HANDLE hThread) {
+    BOOL                result;
+    HANDLE              process;
+    CONTEXT             context;
+    STACKFRAME64        stack;
+
+    RtlCaptureContext(&context);
+    memset(&stack, 0, sizeof(STACKFRAME64));
+
+    process                = GetCurrentProcess();
+    stack.AddrPC.Offset    = context.Rip;
+    stack.AddrPC.Mode      = AddrModeFlat;
+    stack.AddrStack.Offset = context.Rsp;
+    stack.AddrStack.Mode   = AddrModeFlat;
+    stack.AddrFrame.Offset = context.Rbp;
+    stack.AddrFrame.Mode   = AddrModeFlat;
+    std::string Report("Stack for thread ID: ");
+    Report += std::to_string(GetThreadId(hThread)) + '\n';
+    do {
+        result = StackWalk64(
+                IMAGE_FILE_MACHINE_AMD64,
+                process,
+                hThread,
+                &stack,
+                &context,
+                nullptr,
+                SymFunctionTableAccess64,
+                SymGetModuleBase64,
+                nullptr
+                );
+
+        std::string FunctionSym(getFunctionSym(stack.AddrPC.Offset));
+        std::string Location;
+        if(!FunctionSym.empty()) {
+            Location = getLocation(stack.AddrPC.Offset);
+            Report += FunctionSym + " | " + Location + '\n';
+        } else {
+            Report += std::to_string(stack.AddrPC.Offset) + '\n';
+        }
+
+    }while(result);
+    return Report;
+}
+
+
+void generate_crash_report(uint32_t Code, size_t Address) {
     notify("generating crash report, please wait");
-    Init.store(false);
-    heap_array<char> Report(watch_data->size() * sizeof(function_info));
-    Report.string_insert("crash code ");
-    Report.string_insert(HexString(Code).c_str());
-    Report.string_insert(" at ");
-    Report.string_insert(HexString(size_t(Address) + Offset).c_str());
-    Report.string_insert("\n");
+    std::string Report("crash code ");
+    Report += HexString(Code) + " at " + HexString(Address + Offset) + '\n';
     if(Address) {
-        Report.string_insert("origin and line number -> ");
-        Report.string_insert(getCrashInfo(Address).c_str());
-        Report.string_insert("\n");
+        Report += "origin and line number -> " + getLocation(Address) + '\n';
     }
-    Report.string_insert("Call history: \n");
-    char buff[20];
-    for(size_t C = 0; C < watch_data->size(); C++){
-        auto entry = watch_data->at(C);
-        auto Name = FindFunction(entry.func_address);
-        if(entry.enter){
-            Report.string_insert("[Entry] ");
+    Report += "Reports: \n";
+    HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (h != INVALID_HANDLE_VALUE) {
+        THREADENTRY32 te;
+        te.dwSize = sizeof(te);
+        if (Thread32First(h, &te)) {
+            do {
+                if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) +
+                sizeof(te.th32OwnerProcessID)) {
+                    Report += getStack( OpenThread(READ_CONTROL, FALSE, te.th32ThreadID));
+                }
+                te.dwSize = sizeof(te);
+            } while (Thread32Next(h, &te));
         }
-        else {
-            Report.string_insert("[Exit ] ");
-        }
-        auto n = sprintf(buff, "(%d) ", entry.thread_id);
-        Report.string_insert(buff, n);
-        if(Name.size() > 0){
-            Report.string_insert(Name.c_str(), Name.size());
-            Report.string_insert("  |  ");
-            auto location = getCrashInfo(entry.func_address);
-            Report.string_insert(location.c_str(), location.size());
-        }
-        else {
-            Report.string_insert(HexString(size_t(entry.func_address) + Offset).c_str());
-        }
-        Report.string_insert("\n");
+        CloseHandle(h);
     }
-    write_report(Report.cget(), Report.size());
+    write_report(Report);
     notify("crash report generated");
-    Init.store(true);
 }
 
 LONG WINAPI CrashHandler(EXCEPTION_POINTERS* p) {
-    Init.store(false);
     notify("CAUGHT EXCEPTION!");
-    generate_crash_report(p->ExceptionRecord->ExceptionCode, p->ExceptionRecord->ExceptionAddress);
+    generate_crash_report(p->ExceptionRecord->ExceptionCode, size_t(p->ExceptionRecord->ExceptionAddress));
     return EXCEPTION_EXECUTE_HANDLER;
 }
 
-void watchdog_init(const char* crashFile, const char* SpecificPDBLocation, bool Symbols) {
+void watchdog_init(const std::string& crashFile, const char* SpecificPDBLocation, bool Symbols) {
     if(Symbols)SymInitialize(GetCurrentProcess(), SpecificPDBLocation, TRUE);
     Sym.store(Symbols);
     SetUnhandledExceptionFilter(CrashHandler);
-    watch_data = new heap_array<function_info>();
-    watchdog_mutex::Create();
-    crash_file.push_back(crashFile);
+    crash_file = crashFile;
     notify("initialized!");
-    Init.store(true);
-}
-
-inline void AddEntry(void* func_address, uint32_t thread_id, bool entry) {
-    watchdog_mutex::ScopedLock guard;
-    if(Init.load()) {
-        watch_data->insert({func_address, thread_id, entry});
-    }
-}
-
-extern "C" {
-    void FuncEntry(void* func) {
-        AddEntry(func, GetCurrentThreadId(), true);
-    }
-    void FuncExit(void* func) {
-        AddEntry(func, GetCurrentThreadId(), false);
-    }
 }
--- watchdog.h
+++ watchdog.h
@@ -4,9 +4,10 @@
 
 #pragma once
 #include <cstdint>
-extern void watchdog_init(const char* crashFile, const char* SpecificPDBLocation, bool Symbols = true);
-extern void generate_crash_report(uint32_t Code, void* Address);
-const char* getFunctionDetails(void* Address);
+#include <string>
+extern void watchdog_init(const std::string& crashFile, const char* SpecificPDBLocation, bool Symbols = true);
+extern void generate_crash_report(uint32_t Code, size_t Address);
+std::string getFunctionDetails(size_t Address);
 extern void watchdog_setOffset(int64_t Off);
-const char* getCrashLocation(void* Address);
+std::string getCrashLocation(size_t Address);
 void InitSym(const char* PDBLocation);
